b53c97aa953294a79c8deff86721389de1e86c26
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2024-06-24 21:06:00 -0300

    Creacion de esqueletos de controladores

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,13 @@
+class CoursesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,10 @@
+class PurchasesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
