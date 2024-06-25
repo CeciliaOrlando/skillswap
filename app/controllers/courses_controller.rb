834b5647c9948da1388df4dfdcb62b0131fd9c4f
Sofia Torres <torresc.sof@gmail.com>
2024-06-25 14:28:25 -0400

    Avance de courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,51 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  
+  def index
+    @courses = Course.all
+  end
+
   def show
+    @course = Course.find(params[:id])
   end
 
   def new
+    @course = Course.new
   end
 
-  def index
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @course = Course.find(params[:id])
   end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(course_params)
+    redirect_to courses_path
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path, status: :see_other
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:name, :description, :price, :user_id)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
 end
