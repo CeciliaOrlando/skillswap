a6423c507a4ab1928208f51fcb286079bc53227f
der Freidenker <barranzuelald@gmail.com>
2024-06-26 13:24:12 -0500

    controller-purchases, model purchase, view purchase 15%

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,4 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-
   def index
     @purchases = Purchase.where(user: current_user)
   end
@@ -11,43 +9,25 @@ class PurchasesController < ApplicationController
 
   def new
     @purchase = Purchase.new
+    @course = Course.find(params[:course_id])
   end
 
   def create
-    @course = Purchase.find(params[:id])
-    @purchase = Purchase.new(purchase_params)
+    @course = Course.find(params[:course_id])
+    @purchase = Purchase.new
     @purchase.user = current_user
     @purchase.course = @course
-    @purchase.price = @course.price
-    if @purchase.save
+    @purchase.amount = @course.price
+    if @purchase.save!
       redirect_to purchases_path
     else
-      render 'new', status: :unprocessable_entity
-    end
-  end
-
-  def edit
-    @purchase = Purchase.find(params[:id])
-  end
-
-  def update
-    @purchase = Purchase.find(params[:id])
-    if @purchase.update(purchase_params)
-      redirect_to @purchases
-    else
-      render 'edit', status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     @purchase = Purchase.find(params[:id])
     @purchase.destroy
-    redirect_to purchases_path
-  end
-
-  private
-
-  def purchase_params
-    params.require(:purchase).permit(:user_id, :course_id)
+    redirect_to purchases_path, status: :see_other
   end
 end
