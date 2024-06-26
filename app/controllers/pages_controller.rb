c5b1e22665aff1ff9f0872584acf0426ee01a726
alejandra gonzalez <lagonzalez.grafica@gmail.com>
2024-06-26 18:46:03 -0500

    courses cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @courses = Course.all
+    @courses = Course.first(4)
   end
 end
